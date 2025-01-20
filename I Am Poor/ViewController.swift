10385e968d1b21834938f3d25752057edbf8261e
F3858208 Guilherme Carneiro De Oliveira Martins <gcomartins@gmail.com>
2025-01-20 08:17:00 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Guilherme Martins on 20/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by Guilherme Martins on 20/01/25.
+//
+
+import Testing
+@testable import I_Am_Poor
+
+struct I_Am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
